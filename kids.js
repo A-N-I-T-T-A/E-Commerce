673b1aa9219c25fd4634e11a07ef8c9e0788fa4f
A-N-I-T-T-A <133679742+A-N-I-T-T-A@users.noreply.github.com>
2023-11-20 11:31:02 +0530

    Add files via upload

---
+++ kids.js
@@ -0,0 +1,30 @@
+function increment(){
+    var x=Number(document.getElementById('qty').value)
+    x=x+1;
+    document.getElementById("qty").value=x;
+}
+function decrement(){
+    var x=Number(document.getElementById('qty').value)
+    x=x-1;
+    document.getElementById("qty").value=x;
+}
+function increment1(){
+    var x=Number(document.getElementById('qty1').value)
+    x=x+1;
+    document.getElementById("qty1").value=x;
+}
+function decrement1(){
+    var x=Number(document.getElementById('qty1').value)
+    x=x-1;
+    document.getElementById("qty1").value=x;
+}
+function increment2(){
+    var x=Number(document.getElementById('qty2').value)
+    x=x+1;
+    document.getElementById("qty2").value=x;
+}
+function decrement2(){
+    var x=Number(document.getElementById('qty2').value)
+    x=x-1;
+    document.getElementById("qty2").value=x;
+}
